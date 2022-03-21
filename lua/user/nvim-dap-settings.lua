c65357ca4a5567004c55dd6276842d610349c781
derekthecool <derekthecool@gmail.com>
2022-03-21 11:14:04 -0600

    Remove dap settings for now

--- lua/user/nvim-dap-settings.lua
+++ lua/user/nvim-dap-settings.lua
@@ -8,13 +8,13 @@ map("n" , "<leader>db" , "<cmd>lua require('dap').toggle_breakpoint()<CR>")
 map("n" , "<leader>dr" , "<cmd>lua require('dap').repl.open()<CR>")
 map("n" , "<leader>dR" , "<cmd>lua require('dap').run_last()<CR>")
 
-local dap = require('dap')
+--[[ local dap = require('dap')
 dap.adapters.csharp = {
     type = 'executable';
     command = os.getenv('HOME') .. '/scoop/apps/netcoredbg/2.0.0-895/netcoredbg.exe';
     args = { 'args for my app'};
 }
-
+]]
 -- Mappings to look at later
 -- map('n' , '' , 'lua require'dap'<cmd>.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>')
 -- map('n' , '' , 'lua require'dap'<cmd>.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>')
