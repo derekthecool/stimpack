58962199f63b462936f09d127d821c7171f356e5
derekthecool <derekthecool@gmail.com>
2022-08-04 09:29:42 -0600

    Add random color picker function NewRandomColorscheme

---
+++ NeovimLearningPlayground/colors.lua
@@ -0,0 +1,27 @@
+-- Read current settings
+local colors = vim.api.nvim_exec('colorscheme', true)
+local background = vim.o.background
+print(colors, background)
+
+local count = 0
+local count1 = 0
+local count2 = 0
+-- List all available colorschemes (and exclude light themes)
+local all_colors = vim.fn.getcompletion('', 'color')
+for _, color in pairs(all_colors) do
+  for match in string.gmatch(color, '.*[^light-]+') do
+    print(match)
+    count = count + 1
+  end
+
+  for match in string.gmatch(color, '%w+-%w+-%w+') do
+    print(match)
+    count1 = count1 + 1
+  end
+
+  if not color:match('-light') then
+    print(color)
+    count2 = count2 + 1
+  end
+end
+print(count, count1, count1)
--- lua/stimpack/visualsettings.lua
+++ lua/stimpack/visualsettings.lua
@@ -3,17 +3,35 @@ vim.o.termguicolors = true -- Needed for better color display
 vim.wo.cursorcolumn = true
 vim.wo.colorcolumn = "80" -- Display color column at 80 characters
 
+-- Set the chars that are displayed for trailing whitespace, line overflows, and tab chars
+vim.wo.list = true -- enable the feature
+vim.go.listchars = 'trail:·,precedes:«,extends:»,tab:▸\\ ' -- specify chars
+
 -- vim.cmd "colorscheme base16-atelier-forest" -- Current color scheme
 vim.cmd "colorscheme base16-atelier-sulphurpool" -- Former favorite
 
-vim.cmd([[match Error /\t/]]) -- Diplay tabs with error highlight
 vim.cmd([[match ColorColumn /\s\+$/]]) -- Highlight trailing whitespace with color column
--- vim.cmd "set listchars=trail:·,precedes:«,extends:»,tab:▸\\ "
-vim.go.listchars = 'trail:·,precedes:«,extends:»,tab:▸\\ '
-vim.wo.list = true
 
 require 'nvim-web-devicons'.setup {
   -- globally enable default icons (default to false)
   -- will get overriden by `get_icons` option
   default = true;
 }
+
+-- Function to select a color scheme from my installed list with some filtering
+function NewRandomColorscheme()
+
+  local color_list = {}
+  local all_colors = vim.fn.getcompletion('', 'color')
+  for _, color in pairs(all_colors) do
+    if not color:match('-light') then
+      table.insert(color_list, #color_list, color)
+    end
+  end
+
+  local random_index = math.random(1, #color_list)
+  local new_color = color_list[random_index]
+  vim.notify('Setting colorscheme to: ' .. new_color)
+  vim.cmd(string.format('colorscheme %s', new_color))
+
+end
