5fc7025b07d55490f63be2333fce67d3f513c681
derekthecool <derekthecool@gmail.com>
2022-08-09 14:43:56 -0600

    Add csharp treesitter code symbols

---
+++ after/queries/c_sharp/highlights.scm
@@ -0,0 +1,18 @@
+;; Keywords
+(("case"       @keyword)   (#set!   conceal   "💼"))
+(("const"      @keyword)   (#set!   conceal   "⚪"))
+(("default"    @keyword)   (#set!   conceal   "❓"))
+(("do"         @keyword)   (#set!   conceal   "⚒"))
+(("else"       @keyword)   (#set!   conceal   "!"))
+(("enum"       @keyword)   (#set!   conceal   "🔢"))
+(("for"        @keyword)   (#set!   conceal   ""))
+(("if"         @keyword)   (#set!   conceal   "?"))
+(("return"     @keyword)   (#set!   conceal   ""))
+(("while"      @keyword)   (#set!   conceal   "🔁"))
+(("break"      @keyword)   (#set!   conceal   "🔨"))
+
+
+; ((predefined_type)      @TSType   (#set!   conceal   "×"))
+
+;; Sets all types (int, char etc. to this symbol)
+;; ((primitive_type)      @TSType   (#set!   conceal   "×"))
