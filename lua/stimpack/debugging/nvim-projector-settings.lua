11ebb36ea618e5d658ae7fefbe81df5b9103c27e
derekthecool <derekthecool@gmail.com>
2022-10-15 16:08:23 -0600

    Update settings properly to new projector setup

--- lua/stimpack/debugging/nvim-projector-settings.lua
+++ lua/stimpack/debugging/nvim-projector-settings.lua
@@ -1,31 +1,44 @@
 -- takes an optional argument for path, default is './.vim/projector.json'
 -- require('projector.config_utils').load_project_configurations('./projector.json')
 -- require('projector.config_utils').load_dap_configurations()
+P('debugging')
+
+--[[
+Issues with new refactor projector
+-- Legacy
+- Parsing legacy json config reported success but then gave me a empty file
+- After the parsing fairliure it throws an error that it could not parse json
+
+-- New built in
+
+]]
 
 require('projector').setup({
-  -- array of loader names with parameters (for available outputs see LOADERS.md)
   loaders = {
     {
       module = 'builtin',
-      opt = vim.fn.getcwd() .. 'projector.json',
+      opt = vim.fn.getcwd() .. '/projector.json',
     },
     {
-      module = 'dap',
-      opt = '',
+      module = 'legacy.json',
+      opt = vim.fn.getcwd() .. '/old_projector.json',
+    },
+    {
+      module = 'builtin',
+      opt = vim.fn.getcwd() .. '/.vscode/projector.json',
     },
   },
-  -- map of outputs per mode (for available outputs see OUTPUTS.md)
+  display_format = function(_, scope, group, modes, name)
+    return scope .. '  ' .. group .. '  ' .. modes .. '  ' .. name
+  end,
+  automatic_reload = true,
+
   outputs = {
     task = 'builtin',
     debug = 'dap',
     database = 'dadbod',
   },
-  -- function that formats the task selector output
-  display_format = function(loader, scope, group, modes, name)
-    return loader .. '  ' .. scope .. '  ' .. group .. '  ' .. modes .. '  ' .. name
-  end,
   -- Reload configurations automatically before displaying task selector
-  automatic_reload = false,
   -- map of icons
   -- NOTE: "groups" use nvim-web-devicons if available
   icons = {
