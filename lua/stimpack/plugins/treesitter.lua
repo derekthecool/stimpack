054a7fe4fa4cc7074f301b68ac1f198dec2f3f2d
Derek <dlomax@freeus.com>
2023-02-24 11:42:50 -0700

    Add treesitter update all

--- lua/stimpack/plugins/treesitter.lua
+++ lua/stimpack/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
         'nvim-treesitter/nvim-treesitter-textobjects',
         'nvim-treesitter/playground',
     },
-    build = ':TSUpdate',
+    build = ':TSUpdate all',
     event = 'VeryLazy',
     config = function()
         require('nvim-treesitter.configs').setup({
