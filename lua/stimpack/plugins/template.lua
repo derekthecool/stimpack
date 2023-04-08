5167b160e6b3e0af365789dc6d32a8dbf19fb34a
Derek Lomax <derekthecool@gmail.com>
2023-04-08 13:19:01 -0600

    Add template plugin and a simple template for vimwiki diary files

---
+++ lua/stimpack/plugins/template.lua
@@ -0,0 +1,26 @@
+return {
+    'glepnir/template.nvim',
+    config = function()
+        vim.keymap.set('n', '<leader>fT', ':Telescope find_template<CR>', { silent = true })
+
+        require('template').setup({
+            temp_dir = OS.join_path({ vim.fn.stdpath('config'), 'template.nvim_templates' }),
+            author = 'Derek Lomax',
+            email = 'derekthecool@gmail.com',
+            project = {
+                ['test'] = {
+                    ['lang'] = {
+                        ['lua'] = {
+                            ['lua'] = {
+                                'plugin.lua',
+                            },
+                            ['tests'] = {
+                                'plugin_spec.lua',
+                            },
+                        },
+                    },
+                },
+            },
+        })
+    end,
+}
---
+++ template.nvim_templates/vimwiki_diary_starter.md
@@ -0,0 +1,14 @@
+# {{_lua:os.date('%Y-%m-%d')_}} Notes
+
+## Work Objectives
+
+### Planned
+
+- [ ] {{_cursor_}}
+
+## Rock Progress
+
+- [FloatingTodoList](FloatingTodoList)
+- [Rocks](../rocks/Rocks.md)
+
+## Notes
