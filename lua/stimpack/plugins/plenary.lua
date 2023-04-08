00c1f1953cc775a8a040d14e6924aa4761adf8cf
Derek Lomax <derekthecool@gmail.com>
2023-04-08 13:26:13 -0600

    Add my custom plenary patch for windows test harness

--- lua/stimpack/plugins/plenary.lua
+++ lua/stimpack/plugins/plenary.lua
@@ -1,4 +1,12 @@
 return {
-    'nvim-lua/plenary.nvim',
+    -- 'nvim-lua/plenary.nvim',
+
+    -- Use my custom fork which enables test harness on windows
+    -- if this pull request ever gets merged then I'll switch back
+    -- https://github.com/nvim-lua/plenary.nvim/pull/476
+    'derekthecool/plenary.nvim',
+    branch = 'patch-1',
     lazy = false,
+    -- dir = [[D:\Programming\neovim\plenary.nvim]],
+    -- dev = true,
 }
