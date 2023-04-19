23318808d30b78a8ace9b7faa82c3300e424cfd3
Derek Lomax <derekthecool@gmail.com>
2023-04-19 10:04:49 -0600

    Work on line crusher booster

--- lua/stimpack/boosters/line-crusher.lua
+++ lua/stimpack/boosters/line-crusher.lua
@@ -1,7 +1,14 @@
 local M = {}
 
-M.crush_current_line = function()
-     return 'test'
+M.crush_current_line = function(buffer)
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
+
+    print(string.format('crush buffer number is = %d', buffer))
+    print(string.format('Line is = %s, cursor is = %d,%d', vim.inspect(lines), cursor[1], cursor[2]))
+    -- lines[1] = lines[1]:gsub('^wwww', 'xxxx')
+    local replacement = lines
+    vim.api.nvim_buf_set_lines(buffer, cursor[1], cursor[1], false, replacement)
 end
 
 return M
--- lua/tests/stimpack/boosters/line-crusher_spec.lua
+++ lua/tests/stimpack/boosters/line-crusher_spec.lua
@@ -26,22 +26,43 @@ describe('Stimpack line-crusher --', function()
         end)
     end)
 
+    -- TODO: issues with buffer text, fix me
     it('crush_current_line text should remain unchanged', function()
         -- Create new temp file
+        local buffer = vim.api.nvim_create_buf(true, true)
+        print(string.format('Test buffer number is = %d', buffer))
+        print(buffer)
+        assert.is_not.equal(buffer, 0)
 
         -- Set text width to 80 chars
+        vim.opt.textwidth = 80
 
         -- Set exactly 80 chars of text
+        local line = { 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww' }
+        vim.api.nvim_buf_set_lines(buffer, 0, 0, true, line)
+        vim.cmd('normal gg')
 
         -- Run crush_current_line
+        crush.crush_current_line(buffer)
 
         -- Verify that file still only has one line
+        local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, true)
+        print(vim.inspect(line))
+        print(line)
+        print(vim.inspect(lines))
+        print(lines)
+
+        -- This checks value for equality
+        assert.are.same(line, lines)
+
+        -- This checks reference equality, which should not match
+        assert.is_not.equal(line, lines)
 
         -- Optionally check that contents are the same as well
 
         -- Dummy test for now
-        assert.has_no_errors(function()
-            crush.crush_current_line()
-        end)
+        -- assert.has_no_errors(function()
+        --     crush.crush_current_line()
+        -- end)
     end)
 end)
