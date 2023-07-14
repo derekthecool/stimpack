642c9bb8bae2c627ebee9a3870d24fa13d2d3e9e
Derek Lomax <derekthecool@gmail.com>
2023-07-14 13:29:43 -0600

    test: Add some tests with luafun library

--- NeovimLearningPlayground/functional-programming-luafun.lua
+++ NeovimLearningPlayground/functional-programming-luafun.lua
@@ -14,3 +14,16 @@ local two = fun.range(50)
 local three = fun.range(50)
 
 fun.zip(one, two, three):each(print)
+
+-- A basic thing I want is to take a single number and create a list with mappings and sum them
+local sum_test = fun.sum(fun.range(10))
+print(sum_test)
+print(type(sum_test))
+
+local sum_test2 = fun.range(10):map(function(x) return x^2 end):sum()
+print(sum_test2)
+print(type(sum_test2))
+
+local sum_test3 = (fun.range(10):sum() ^ 2)
+print(sum_test3)
+print(type(sum_test3))
