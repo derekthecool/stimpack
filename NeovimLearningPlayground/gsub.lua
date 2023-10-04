2faa67541ecdf2d2549a648405c309b090bca3bb
Derek Lomax <derekthecool@gmail.com>
2023-10-04 10:20:47 -0600

    Add refactoring plugin

--- NeovimLearningPlayground/gsub.lua
+++ NeovimLearningPlayground/gsub.lua
@@ -1,4 +1,4 @@
-local title_casing = "This Is Title Casing"
+local title_casing = 'This Is Title Casing'
 
 local function invert_case(s)
     if s:byte() < string.byte('a') then
@@ -18,3 +18,12 @@ local function to_snake_case(s)
 end
 print((title_casing:gsub(' ?%u', to_snake_case)))
 print((title_casing:gsub(' ', '_'):lower()))
+
+local replacement_table = {
+    dog = 'cat',
+    cow = 'sheep',
+    monkey = 'harpy eagle',
+}
+
+local original_text = "the dog met a cow who had been attacked by a monkey"
+print(original_text:gsub('%w+',replacement_table))
