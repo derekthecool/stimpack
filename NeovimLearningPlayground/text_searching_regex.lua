305930fd708f55fd18995e5a898adec4c057ac73
Derek Lomax <derekthecool@gmail.com>
2024-02-16 15:10:42 -0700

    Add more awesome stuff for log filtering

--- NeovimLearningPlayground/text_searching_regex.lua
+++ NeovimLearningPlayground/text_searching_regex.lua
@@ -1,5 +1,15 @@
-local testText = 'This is test text 1234'
+local testText = 'This is test text 1234 + 555 = 777'
 print(testText)
+
+-- Lua matching
+local lua_match = testText:match('test')
+print(lua_match)
+
+-- Lua matching
+local lua_match = testText:match('%+')
+print(lua_match)
+
+-- Full vim regex
 
 -- Set the \v very magic, back slashes need to be escaped
 local match = vim.regex('\\v\\d'):match_str(testText)
@@ -8,6 +18,27 @@ print(match)
 local check = vim.regex('print'):match_line(0, 1)
 print(check)
 
+local regex = vim.regex('.*')
+print(regex)
+print(vim.inspect(regex))
+print(getmetatable(regex))
+
+local matchedString = vim.fn.matchstr(testText, '\\v(\\w+).*(\\d+)')
+print(matchedString)
+print(type(matchedString))
+
+local matchedString = vim.fn.matchstr(testText, '777')
+print(matchedString)
+print(type(matchedString))
+
+local matchedList = vim.fn.matchlist(testText, '777')
+print(matchedList)
+print(next(matchedList))
+print(type(matchedList))
+
+local matchedList = vim.fn.matchlist(testText, '\\v\\d')
+print(matchedList)
+
 local current_buffer = 0
 local start_line = 0
 local last_line = -1
@@ -15,7 +46,7 @@ local strict_indexing = false
 local lines = vim.api.nvim_buf_get_lines(current_buffer, start_line, last_line, strict_indexing)
 print(type(lines))
 for line_number, line in pairs(vim.api.nvim_buf_get_lines(current_buffer, start_line, last_line, strict_indexing)) do
-  print(line)
+    print(line)
 end
 
 -- Print the line count in the current buffer
@@ -48,4 +79,4 @@ print(string.format('Lines in current buffer = %d', vim.api.nvim_buf_line_count(
     end
 end ]]
 
-print(string.match("This is a test", '^.*$'))
+print(string.match('This is a test', '^.*$'))
--- ftdetect/mcn_device_logs.lua
+++ ftdetect/mcn_device_logs.lua
@@ -1,12 +1,61 @@
 -- Define the patterns to match
+-- local log_level = vim.diagnostic.severity.ERROR
 local patterns_to_match = {
-    'MTpub',
-    'tcpreceive',
-    'MTrecv',
-    'MQTT',
-    'FA:',
-    'F1:',
+    { pattern = 'MTpub',                                  level = vim.diagnostic.severity.INFO },
+    { pattern = 'tcpreceive',                             level = vim.diagnostic.severity.INFO },
+    { pattern = 'MTrecv',                                 level = vim.diagnostic.severity.INFO },
+    { pattern = 'MQTT',                                   level = vim.diagnostic.severity.INFO },
+    { pattern = 'FA:',                                    level = vim.diagnostic.severity.INFO },
+    { pattern = 'F1:',                                    level = vim.diagnostic.severity.INFO },
+    { pattern = 'writeid',                                level = vim.diagnostic.severity.INFO },
+    { pattern = 'CME Error',                              level = vim.diagnostic.severity.ERROR },
+    { pattern = 'PA30V01.01B%d%d %d%d%d%d',               level = vim.diagnostic.severity.INFO },
+
+    -- Fota
+    { pattern = 'enter rsfota',                           level = vim.diagnostic.severity.INFO },
+    { pattern = 'AT%+STRTO=PA30,8',                       level = vim.diagnostic.severity.INFO },
+    { pattern = '%+ACK:STRTO.*FOTA',                      level = vim.diagnostic.severity.INFO },
+    { pattern = 'Content-Range: bytes',                   level = vim.diagnostic.severity.INFO },
+    { pattern = 'TrackerPa23ReportSTHFS:',                level = vim.diagnostic.severity.INFO },
+    { pattern = 'send %(fota.redstone.net.cn%) complete', level = vim.diagnostic.severity.INFO },
 }
+
+local function log_lsp_scanner(input_patterns)
+    V('Scanning patterns')
+    -- Create namespace and clear it so old errors go away
+    local source = 'LogErrorChecker'
+    local namespace = vim.api.nvim_create_namespace(source)
+    local buffer_number = 0
+    vim.api.nvim_buf_clear_namespace(buffer_number, namespace, 0, -1)
+
+    -- Check every line for similar errors
+    -- Match this one
+    -- 2023-06-12 12:36:21.555 -06:00 [DBG] +CME ERROR: 4
+    -- Don't match this one
+    -- 2023-06-12 12:36:21.557 -06:00 [DBG] TelitParser_ResultCodes-+CME ERROR: 4,13,curr_part=0,total_part=1    T:(12:33:22)
+    local lines = vim.api.nvim_buf_get_lines(buffer_number, 0, -1, false)
+    local diagnostic_error_list = {}
+    for linenumber, line in ipairs(lines) do
+        for _, pattern_input_item in pairs(input_patterns) do
+            local match_found = line:match(pattern_input_item.pattern)
+
+            if match_found then
+                local diagnostic_message = {
+                    bufnr = buffer_number,
+                    lnum = linenumber - 1,
+                    col = 0,
+                    severity = pattern_input_item.level,
+                    source = source,
+                    -- message = string.format('CME Error %s', match_found),
+                    message = '',
+                    user_data = {},
+                }
+                table.insert(diagnostic_error_list, diagnostic_message)
+            end
+        end
+    end
+    vim.diagnostic.set(namespace, buffer_number, diagnostic_error_list, {})
+end
 
 -- Function to determine if a line should be folded
 function mcn_fold_lines()
@@ -65,43 +114,26 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
 
         -- Easily start searching for any of these items
         vim.keymap.set('n', '<leader>nm', function()
+            local function startSearch(text)
+                -- Escape any special characters in the search text
+                -- local escapedText = vim.fn.escape(text, '/')
+                -- Construct the search command
+                local searchCommand = '/\\v' .. text .. '\n'
+                -- Send the search command
+                vim.fn.feedkeys(searchCommand, 'n')
+            end
             local search_string = table.concat(patterns_to_match, '|')
-            vim.cmd(string.format('\v%s', search_string))
+            -- vim.cmd(string.format('\\v(%s)', search_string))
+            startSearch(search_string)
         end, { silent = true, desc = 'mcn search' })
 
         -- TODO: move this to a stimpack booster with custom options to make it easy to scan any file
 
         vim.keymap.set('n', '<leader>lL', function()
-            -- Create namespace and clear it so old errors go away
-            local namespace = vim.api.nvim_create_namespace('LogErrorChecker')
-            local buffer_number = 0
-            vim.api.nvim_buf_clear_namespace(buffer_number, namespace, 0, -1)
-
-            -- Check every line for similar errors
-            -- Match this one
-            -- 2023-06-12 12:36:21.555 -06:00 [DBG] +CME ERROR: 4
-            -- Don't match this one
-            -- 2023-06-12 12:36:21.557 -06:00 [DBG] TelitParser_ResultCodes-+CME ERROR: 4,13,curr_part=0,total_part=1    T:(12:33:22)
-            local lines = vim.api.nvim_buf_get_lines(buffer_number, 0, -1, false)
-            local diagnostic_error_list = {}
-            for linenumber, line in ipairs(lines) do
-                local error = line:match('%+CME ERROR: (%d+)$')
-                if error then
-                    -- vim.notify('error = ' .. (error or 'nil'), vim.log.levels.INFO, { title = 'Stimpack Notification' })
-                    local diagnostic_message = {
-                        bufnr = buffer_number,
-                        lnum = linenumber - 1,
-                        col = 0,
-                        severity = vim.diagnostic.severity.ERROR,
-                        source = 'LogErrorChecker',
-                        message = string.format('CME Error %s', error),
-                        user_data = {},
-                    }
-                    table.insert(diagnostic_error_list, diagnostic_message)
-                end
-            end
-            vim.diagnostic.set(namespace, buffer_number, diagnostic_error_list, {})
+            log_lsp_scanner(patterns_to_match)
         end, { silent = true, desc = 'LogErrorChecker find CME errors', buffer = true })
+
+        log_lsp_scanner(patterns_to_match)
     end,
     group = vim.api.nvim_create_augroup('mcn_device_logs', { clear = true }),
 })
