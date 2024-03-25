11c4e3d88ef85b129dc053a2796775b4c153207a
Derek Lomax <derekthecool@gmail.com>
2024-03-24 21:26:21 -0600

    Move two of my plugin maps into the experimental plugin file

--- lua/stimpack/plugins/experimental-plugin-testing.lua
+++ lua/stimpack/plugins/experimental-plugin-testing.lua
@@ -4,4 +4,45 @@ return {
         'dstein64/vim-startuptime',
         cmd = 'StartupTime',
     },
+
+    -- My plugins{
+    -- https://github.com/derekthecool/dvp.nvim
+    {
+        'derekthecool/dvp.nvim',
+        event = 'CursorMoved',
+        config = function()
+            vim.keymap.set('n', '<leader>nn', require('dvp').comma_count, { desc = 'Comma count' })
+            vim.keymap.set('n', '<leader>nb', require('dvp').bit_flip, { desc = 'Bit flip' })
+        end,
+    },
+
+    -- https://github.com/derekthecool/plover-tapey-tape.nvim
+    {
+        'derekthecool/plover-tapey-tape.nvim',
+        event = 'CursorMoved',
+        enabled = false, -- There are still bugs with fresh installs that I need to fix
+        config = function()
+            require('plover-tapey-tape').setup({
+                filepath = 'auto',
+                open_method = 'vsplit',
+                vertical_split_height = 9,
+                horizontal_split_width = 54,
+                steno_capture = '|(.-)|',
+                suggestion_notifications = {
+                    enabled = true,
+                },
+                status_line_setup = {
+                    enabled = true,
+                    additional_filter = '(|.-|)',
+                },
+            })
+
+            vim.keymap.set(
+                'n',
+                '<leader>nt',
+                require('plover-tapey-tape').toggle,
+                { desc = 'Toggle plover-tapey-tape' }
+            )
+        end,
+    },
 }
