97c91ed45c07c4261971e122925bf2cbce93fff6
Derek Lomax <derekthecool@gmail.com>
2024-03-24 20:30:21 -0600

    Fix problem with newer neovim not loading transparent coloring plugin

--- lua/stimpack/plugins/colorscheme-base16.lua
+++ lua/stimpack/plugins/colorscheme-base16.lua
@@ -1,8 +1,12 @@
 return {
     'RRethy/nvim-base16',
     priority = 1000, --set high priority for colorscheme
+    dependencies = {
+        'xiyaowong/nvim-transparent',
+    },
     event = 'BufEnter',
     config = function()
         vim.api.nvim_cmd({ cmd = 'colorscheme', args = { 'base16-atelier-sulphurpool' } }, {})
+        vim.api.nvim_cmd({ cmd = 'TransparentEnable' }, {})
     end,
 }
