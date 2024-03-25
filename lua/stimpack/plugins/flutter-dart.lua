ff3f09e58c8b06b94b63ed007ff233b2293dbe13
Derek Lomax <derekthecool@gmail.com>
2024-03-24 21:25:59 -0600

    Move flutter plugin to event lazy for slightly faster loading

--- lua/stimpack/plugins/flutter-dart.lua
+++ lua/stimpack/plugins/flutter-dart.lua
@@ -1,16 +1,6 @@
--- -- https://x-team.com/blog/neovim-flutter/
--- return {
---     {
---         'dart-lang/dart-vim-plugin',
---     },
---     { 'thosakwe/vim-flutter' },
---     { 'natebosch/vim-lsc' },
---     { 'natebosch/vim-lsc-dart' },
--- }
-
 return {
     'akinsho/flutter-tools.nvim',
-    lazy = false,
+    event = 'VeryLazy',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'stevearc/dressing.nvim',
