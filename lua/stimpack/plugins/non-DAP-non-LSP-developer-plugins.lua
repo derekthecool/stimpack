f9755ac7b4432005d82139e855d8e5c32fc8764a
Derek Lomax <derekthecool@gmail.com>
2024-03-26 10:32:42 -0600

    Move luapad into developer plugins spec

--- lua/stimpack/plugins/non-DAP-non-LSP-developer-plugins.lua
+++ lua/stimpack/plugins/non-DAP-non-LSP-developer-plugins.lua
@@ -1,5 +1,36 @@
 return {
     {
+        'rafcamlet/nvim-luapad',
+        -- To jump to the printed preview window, use ^w w
+        keys = {
+            {
+                '<leader>dV',
+                function()
+                    require('luapad').toggle()
+                end,
+                desc = 'Luapad neovim interactive buffer',
+            },
+        },
+        opts = {
+            count_limit = 150000,
+            error_indicator = true,
+            eval_on_move = true,
+            error_highlight = 'TSQueryLinterError',
+            print_highlight = 'DevIconMotoko',
+            split_orientation = 'vertical',
+            on_init = function()
+                print('Hello from Luapad!')
+            end,
+            context = {
+                the_answer = 42,
+                shout = function(str)
+                    return (string.upper(str) .. '!')
+                end,
+            },
+        },
+    },
+
+    {
         'numToStr/Comment.nvim',
         keys = {
             { 'gc', mode = { 'n', 's', 'v' } },
