1dc93be013158282c86eda6e4201d3f1e3cbc954
Derek Lomax <derekthecool@gmail.com>
2024-03-29 09:40:30 -0600

    Add more playground testing for plenary path

--- NeovimLearningPlayground/plenary/plenary_path.lua
+++ NeovimLearningPlayground/plenary/plenary_path.lua
@@ -8,3 +8,14 @@ print(test.filename)
 local file = test:absolute()
 print(file)
 print(vim.fs.normalize(file))
+
+
+-- Debugging neotest plenary runner problems on windows today 2024-03-27
+print((require('plenary.path').new('./bit.lua')):absolute())
+print((require('plenary.path').new('./bit.lua'):parent() / 'test' / 'monkey.txt').filename)
+print(require('plenary.path').new('./bit.lua'):parent():parent().filename)
+print(require('plenary.path').new('./bit.lua'):parent():parent().filename)
+local script_path = debug.getinfo(1, "S").source:sub(2)
+print(script_path)
+local test_script = (path.new(script_path()):parent():parent() / "run_tests.lua").filename
+print(test_script)
