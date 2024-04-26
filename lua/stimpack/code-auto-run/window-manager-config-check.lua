d7c29ff308293c7a1f41bef0bed5a50fcbafdd67
Derek Lomax <derekthecool@gmail.com>
2024-04-26 15:30:52 -0600

    Add better way to check awesome WM code from windows

--- lua/stimpack/code-auto-run/window-manager-config-check.lua
+++ lua/stimpack/code-auto-run/window-manager-config-check.lua
@@ -6,16 +6,29 @@ local autocommand_group = vim.api.nvim_create_augroup('awesome-window-manager-co
 vim.api.nvim_create_autocmd('BufWritePost', {
     pattern = { '*/awesome/rc.lua' },
     callback = function()
-        local output = Execute('awesome --check')
-        -- V(output)
-        if output.ret ~= nil and output.ret == 0 then
-            vim.notify('Awesome WM config OK!', vim.log.levels.INFO, { title = 'Stimpack Notification' })
+        if OS.OS == 'Linux' then
+            local output = Execute('awesome --check')
+            -- V(output)
+            if output.ret ~= nil and output.ret == 0 then
+                vim.notify('Awesome WM config OK!', vim.log.levels.INFO, { title = 'Stimpack Notification' })
+            else
+                vim.notify(
+                    'Awesome WM config failure, DO NOT RESTART AWESOME',
+                    vim.log.levels.ERROR,
+                    { title = 'Stimpack Notification' }
+                )
+            end
         else
-            vim.notify(
-                'Awesome WM config failure, DO NOT RESTART AWESOME',
-                vim.log.levels.ERROR,
-                { title = 'Stimpack Notification' }
-            )
+            local output = Execute('luac -p rc.lua')
+            if output.ret ~= nil and output.ret == 0 then
+                vim.notify('Using just luac, the script is OK', vim.log.levels.INFO, { title = 'Stimpack Notification' })
+            else
+                vim.notify(
+                    'Awesome WM config failure, DO NOT RESTART AWESOME (luac check only)',
+                    vim.log.levels.ERROR,
+                    { title = 'Stimpack Notification' }
+                )
+            end
         end
     end,
     group = autocommand_group,
