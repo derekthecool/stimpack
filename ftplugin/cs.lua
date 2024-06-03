2792ad439b258c048048cfed237cfdb20fd48b75
Derek Lomax <derekthecool@gmail.com>
2024-06-03 11:01:09 -0600

    Add csharp region fold method

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -1,2 +1,21 @@
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
+
+-- Set the fold method to expression
+vim.opt.foldmethod = 'expr'
+-- Define the fold expression
+vim.opt.foldexpr = 'v:lua.CSharpFoldExpr()'
+
+-- Define the fold expression function
+function CSharpFoldExpr()
+    local lnum = vim.v.lnum
+    local line = vim.fn.getline(lnum)
+
+    if string.match(line, '^#region') then
+        return '>1'
+    elseif string.match(line, '^#endregion') then
+        return '<1'
+    else
+        return '='
+    end
+end
