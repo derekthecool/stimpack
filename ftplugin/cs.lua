f341e7f38d435798bdab07f0e532bc3be978dbdb
Derek Lomax <derekthecool@gmail.com>
2024-06-06 14:28:48 -0600

    Add awesome csharp fold from #region blocks

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -4,6 +4,7 @@ vim.o.tabstop = 4
 -- Set the fold method to expression
 vim.opt.foldmethod = 'expr'
 -- Define the fold expression
+-- TODO: (Derek Lomax) 6/3/2024 11:03:02 AM, Could be used for powershell and fsharp as well
 vim.opt.foldexpr = 'v:lua.CSharpFoldExpr()'
 
 -- Define the fold expression function
