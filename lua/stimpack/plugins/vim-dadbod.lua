fb29a621c078cd383ea5384c2823e4910fbd3545
Derek Lomax <derekthecool@gmail.com>
2024-06-12 19:28:17 -0600

    Update dadbod and dbout settings

--- lua/stimpack/plugins/vim-dadbod.lua
+++ lua/stimpack/plugins/vim-dadbod.lua
@@ -6,4 +6,9 @@ return {
     },
     -- Lazy load on these commands
     cmd = { 'DB', 'DBUI', 'DBUIToggle' },
+    config = function()
+        vim.g.db_ui_use_nvim_notify = 1
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.db_ui_show_database_icon = 1
+    end,
 }
