20a8f65f6f60afe75ace7befcbd3cdc10a7cdbca
Derek Lomax <derekthecool@gmail.com>
2024-09-18 13:52:14 -0600

    feat: update projector plugin to handle new features

--- lua/stimpack/plugins/dap.lua
+++ lua/stimpack/plugins/dap.lua
@@ -12,8 +12,6 @@ return {
             '<leader>d',
         },
         config = function()
-            local map = require('stimpack.mapping-function')
-
             require('telescope').load_extension('dap')
 
             require('which-key').add({
@@ -36,17 +34,21 @@ return {
                     desc = 'Logging breakpoint (____)',
                 },
                 { '<leader>dr', '<cmd>lua require(\'dap\').repl.open()<CR>', desc = 'Open repl' },
-                { '<leader>dR', '<cmd>lua require(\'dap\').run_last()<CR>', desc = 'Run last' },
+                { '<leader>dR', '<cmd>lua require(\'dap\').run_last()<CR>',  desc = 'Run last' },
                 { '<leader>dq', '<cmd>lua require(\'dap\').terminate()<CR>', desc = 'Exit debugging' },
-                { '<leader>dl', '<cmd>sp ~/.cache/nvim/dap.log<CR>', desc = 'Open dap log' },
+                { '<leader>dl', '<cmd>sp ~/.cache/nvim/dap.log<CR>',         desc = 'Open dap log' },
             })
 
-            map('n', '≤', '<cmd>lua require(\'dap\').step_over()<CR>')
-            map('n', '≥', '<cmd>lua require(\'dap\').step_into()<CR>')
-            map('n', 'µ', '<cmd>lua require(\'dap\').step_out()<CR>')
-            map('n', '__', '<cmd>lua require(\'dap\').toggle_breakpoint()<CR>')
-            map('n', '___', '<cmd>lua require(\'dap\').set_breakpoint(vim.fn.input(\'Breakpoint condition: \'))<CR>')
-            map(
+            vim.keymap.set('n', '≤', '<cmd>lua require(\'dap\').step_over()<CR>')
+            vim.keymap.set('n', '≥', '<cmd>lua require(\'dap\').step_into()<CR>')
+            vim.keymap.set('n', 'µ', '<cmd>lua require(\'dap\').step_out()<CR>')
+            vim.keymap.set('n', '__', '<cmd>lua require(\'dap\').toggle_breakpoint()<CR>')
+            vim.keymap.set(
+                'n',
+                '___',
+                '<cmd>lua require(\'dap\').set_breakpoint(vim.fn.input(\'Breakpoint condition: \'))<CR>'
+            )
+            vim.keymap.set(
                 'n',
                 '____',
                 '<cmd>lua require(\'dap\').set_breakpoint(nil , nil , vim.fn.input(\'Log point message: \'))<CR>'
@@ -84,18 +86,18 @@ return {
         'theHamsta/nvim-dap-virtual-text',
         lazy = true,
         opts = {
-            enabled = true, -- enable this plugin (the default)
-            enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+            enabled = true,                     -- enable this plugin (the default)
+            enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
             highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-            highlight_new_as_changed = true, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-            show_stop_reason = true, -- show stop reason when stopped for exceptions
-            commented = true, -- prefix virtual text with comment string
+            highlight_new_as_changed = true,    -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+            show_stop_reason = true,            -- show stop reason when stopped for exceptions
+            commented = true,                   -- prefix virtual text with comment string
             -- experimental features:
-            virt_text_pos = 'eol', -- position of virtual text, see `:h nvim_buf_set_extmark()`
-            all_frames = true, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+            virt_text_pos = 'eol',              -- position of virtual text, see `:h nvim_buf_set_extmark()`
+            all_frames = true,                  -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
 
             -- 2022-04-16 virtual lines did not work for me with can clangd
-            virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+            virt_lines = false,      -- show virtual lines instead of virtual text (will flicker!)
             virt_text_win_col = nil, -- position the virtual text at a fixed window column (starting from the first text column) ,
             -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
         },
@@ -145,8 +147,8 @@ return {
                     },
                 },
                 floating = {
-                    max_height = nil, -- These can be integers or a float between 0 and 1.
-                    max_width = nil, -- Floats will be treated as percentage of your screen.
+                    max_height = nil,  -- These can be integers or a float between 0 and 1.
+                    max_width = nil,   -- Floats will be treated as percentage of your screen.
                     border = 'single', -- Border style. Can be "single", "double" or "rounded"
                     mappings = {
                         close = { 'q', '<Esc>' },
@@ -173,57 +175,28 @@ return {
 
     {
         'kndndrj/nvim-projector',
-        enabled = false,
+        dependencies = {
+            -- required:
+            'MunifTanjim/nui.nvim',
+            -- optional extensions:
+            -- 'kndndrj/projector-neotest',
+            -- dependencies of extensions:
+            -- 'nvim-neotest/neotest',
+        },
+        enabled = true,
         keys = {
             '_',
         },
         config = function()
             require('projector').setup({
+
                 loaders = {
-                    {
-                        module = 'builtin',
-                        options = {
-                            -- path = vim.fn.getcwd() .. OS.separator .. 'projector.json',
-                            path = OS.join_path(vim.fn.getcwd(), 'projector.json'),
-                            configs = nil,
-                        },
-                    },
-                },
-                display_format = function(_, scope, group, modes, name)
-                    return scope .. '  ' .. group .. '  ' .. modes .. '  ' .. name
-                end,
-                automatic_reload = true,
-                -- local output_config = require("projector").config.outputs[mode]
-                outputs = {
-                    task = {
-                        module = 'builtin',
-                        options = nil,
-                    },
-                    debug = {
-                        module = 'dap',
-                        options = nil,
-                    },
-                    database = {
-                        module = 'dadbod',
-                        options = nil,
-                    },
-                },
-                -- Reload configurations automatically before displaying task selector
-                -- map of icons
-                -- NOTE: "groups" use nvim-web-devicons if available
-                icons = {
-                    enable = true,
-                    scopes = {
-                        global = '',
-                        project = '',
-                    },
-                    groups = {},
-                    loaders = {},
-                    modes = {
-                        task = '',
-                        debug = '',
-                        database = '',
-                    },
+                    require('projector.loaders').BuiltinLoader:new({
+                        path = function()
+                            return string.format('%s/projector.json', vim.fn.getcwd())
+                        end,
+                    }),
+                    require('projector.loaders').DapLoader:new(),
                 },
             })
 
