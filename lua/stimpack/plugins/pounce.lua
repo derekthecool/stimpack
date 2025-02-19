97488b677228ede1df888570eb0a9845ba01f2d0
Derek Lomax <derekthecool@gmail.com>
2025-02-19 15:45:55 -0700

    feat: remove some pounce mappings

--- lua/stimpack/plugins/pounce.lua
+++ lua/stimpack/plugins/pounce.lua
@@ -1,21 +1,11 @@
 return {
     'rlane/pounce.nvim',
-    keys = { '√', '\\', 's' },
+    keys = { '\\' },
     config = function()
         local map = vim.keymap.set
-        map({ 'n', 'o', 'x' }, 's', function()
-            require('pounce').pounce({})
-        end)
         map({ 'n', 'o', 'x' }, '\\', function()
             require('pounce').pounce({})
         end)
-        -- map('n', 'S', function()
-        --     require('pounce').pounce({ do_repeat = true })
-        -- end)
-        map('n', 'S', function()
-            require('pounce').pounce({ input = { reg = '/' } })
-        end)
-
         require('pounce').setup()
     end,
 }
