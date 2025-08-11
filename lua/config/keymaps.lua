80c6d385591488e0031bdfd5344adcde637ccd4f
Derek Lomax <derekthecool@gmail.com>
2025-08-11 13:22:38 -0600

    feat: make buffer mappings silent

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,9 +29,15 @@ vim.keymap.set({ 'n', 'i' }, '↑', function()
 end)
 
 -- Buffer maps
-vim.keymap.set('', '⊃', ':bnext<cr>') -- Use right mod
-vim.keymap.set('', '⊂', ':bprevious<cr>') -- Use left mod
-vim.keymap.set('', 'π', ':bdelete<cr>') -- Use down mod
+vim.keymap.set('n', '⊃', function() -- Use right Emily's symbols mod
+    vim.api.nvim_cmd({ cmd = 'bnext', mods = { silent = true } }, {})
+end, { desc = 'Next buffer' })
+vim.keymap.set('n', '⊂', function() -- Use left Emily's symbols mod
+    vim.api.nvim_cmd({ cmd = 'bprevious', mods = { silent = true } }, {})
+end, { desc = 'Previous buffer' })
+vim.keymap.set('n', 'π', function() -- Use down Emily's symbols mod
+    vim.api.nvim_cmd({ cmd = 'bdelete', mods = { silent = true } }, {})
+end, { desc = 'Next buffer' })
 
 -- Ｖｉｓｕａｌ ｍｏｄｅ
 -- Better command to shift text in visual mode, text it reselected
