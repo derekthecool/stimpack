ab69ea734eb43c9f3aa0312d2d170d4411038c22
Derek Lomax <derekthecool@gmail.com>
2025-08-22 10:48:32 -0600

    feat: change snacks.picker default layout to ivy_split

--- lua/plugins/basics.lua
+++ lua/plugins/basics.lua
@@ -1,5 +1,11 @@
 return {
     {
+        -- pounce is my favorite quick jumper since it works well with stenography
+        -- oftentimes I can write the entire word I want to jump to quicker than 2-3 chars of that word
+        -- e.g. to jump to the word 'great' with pounce I can just press my '\' keymap and then write
+        -- the entire word 'great' with one single steno stroke and then press the suggestion letter to accept.
+        -- To do the same thing with flash/hop and friends I would have to fingerspell several letters of the
+        -- word which is much slower!
         'rlane/pounce.nvim',
         keys = { '\\' },
         config = function()
@@ -9,8 +15,16 @@ return {
             require('pounce').setup()
         end,
     },
+    { 'echasnovski/mini.pairs', enabled = false },
+    { 'folke/flash.nvim', enabled = false },
     {
-        'echasnovski/mini.pairs',
-        enabled = false,
+        'folke/snacks.nvim',
+        opts = {
+            picker = {
+                layout = {
+                    preset = 'ivy_split',
+                },
+            },
+        },
     },
 }
