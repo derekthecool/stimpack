2f9fbd86d51f75e1ec161d980ab1d4fd7860f0eb
Derek Lomax <derekthecool@gmail.com>
2025-11-15 16:37:06 -0700

    Set localleader and shiftwidth

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ','
+vim.g.maplocalleader = '-' -- Default is \ which I really like for pounce plugin
 
 vim.o.scrolloff = 999 -- Keep cursor vertically centered
 vim.opt.mouse = ''
 vim.opt.wrap = false
+vim.opt.shiftwidth = 4
 
 -- Set pwsh as default terminal
 LazyVim.terminal.setup('pwsh')
